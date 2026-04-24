98dfd6f6e51dbdf92285d9ce886dbc652ed6224f
Adam Douglass <Adam.Douglass@cyber.gc.ca>
2026-04-24 18:31:48 +0000

    move some code into its own module

---
+++ assemblyline-search/src/lib.rs
@@ -0,0 +1,11 @@
+#![allow(
+    // allow these as they sometimes improve clarity
+    clippy::needless_return,
+    clippy::collapsible_else_if
+)]
+
+// pub mod index;
+pub mod lucene;
+pub mod json;
+#[cfg(test)]
+mod tests;
